0f6904fa6b1d5c71d15c2ffb0c10af3a4eed3c6c
HillGnur <murilo.gamboa.melo@gmail.com>
2025-07-24 11:30:11 -0300

    Remoção dos paths do chrome driver para teste

--- conftest.py
+++ conftest.py
@@ -6,9 +6,7 @@ from selenium import webdriver
 
 def browser():
     # Configuração do webdriver
-    chrome_path = '/home/mmelo/chrome-linux64/chrome'
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = chrome_path
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     driver = webdriver.Chrome(options=chrome_options)
