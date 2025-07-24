24d45f42d320668832e844543e6228a27adfd227
HillGnur <murilo.gamboa.melo@gmail.com>
2025-07-24 11:24:08 -0300

    Adição do path do chromedriver para teste

--- conftest.py
+++ conftest.py
@@ -9,7 +9,7 @@ def browser():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
-    driver = webdriver.Chrome(options=chrome_options)
+    driver = webdriver.Chrome('/home/mmelo/chrome-linux64/chrome', options=chrome_options)
 
     yield driver
     driver.quit()
