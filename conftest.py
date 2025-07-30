13118872020c27007fd12d41f2c8acd0e2463151
HillGnur <murilo.gamboa.melo@gmail.com>
2025-07-30 16:44:11 -0300

    no sandbox added on selenium tests

--- conftest.py
+++ conftest.py
@@ -12,6 +12,7 @@ def browser():
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--no-sandbox")
     service=Service(executable_path="/usr/bin/chromedriver")
     driver=webdriver.Chrome(service=service, options=chrome_options)
 
