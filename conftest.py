8c12c7ca5795ac208ef72e9b62fc24d2d8953a59
HillGnur <murilo.gamboa.melo@gmail.com>
2025-08-05 15:25:28 -0300

    Correção do path

--- conftest.py
+++ conftest.py
@@ -13,7 +13,7 @@ def browser():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
-    service=Service(executable_path="/home/mmelo/chrome-linux64/chrome")
+    service=Service(executable_path="/usr/bin/chromedriver")
     driver=webdriver.Chrome(service=service, options=chrome_options)
 
     yield driver
